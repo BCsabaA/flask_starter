e4d75bbe067d2939291da387ac16929f3f9cd040
csabi <barabas.csabi@gmail.com>
2026-04-05 23:16:17 +0200

    is_admin property added to user model

--- app/models.py
+++ app/models.py
@@ -7,9 +7,13 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(256), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
+
+    def set_admin(self):
+        self.is_admin = True
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
